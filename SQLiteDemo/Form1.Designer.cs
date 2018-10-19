e86468d1a27655af0c5392bf7418b6dc03beed76
Xantiros <xantiros@gmail.com>
2018-10-19 11:50:27 +0200

    Preparation

--- SQLiteDemo/Form1.Designer.cs
+++ SQLiteDemo/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.listPeopleListBox = new System.Windows.Forms.ListBox();
+            this.btnAddPerson = new System.Windows.Forms.Button();
+            this.btnRefreshList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(49, 52);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(156, 20);
+            this.tbFirstName.TabIndex = 0;
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(49, 109);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(156, 20);
+            this.tbLastName.TabIndex = 1;
+            // 
+            // listPeopleListBox
+            // 
+            this.listPeopleListBox.FormattingEnabled = true;
+            this.listPeopleListBox.Location = new System.Drawing.Point(250, 52);
+            this.listPeopleListBox.Name = "listPeopleListBox";
+            this.listPeopleListBox.Size = new System.Drawing.Size(217, 225);
+            this.listPeopleListBox.TabIndex = 2;
+            // 
+            // btnAddPerson
+            // 
+            this.btnAddPerson.Location = new System.Drawing.Point(49, 175);
+            this.btnAddPerson.Name = "btnAddPerson";
+            this.btnAddPerson.Size = new System.Drawing.Size(156, 39);
+            this.btnAddPerson.TabIndex = 3;
+            this.btnAddPerson.Text = "AddPerson";
+            this.btnAddPerson.UseVisualStyleBackColor = true;
+            this.btnAddPerson.Click += new System.EventHandler(this.btnAddPerson_Click);
+            // 
+            // btnRefreshList
+            // 
+            this.btnRefreshList.Location = new System.Drawing.Point(49, 238);
+            this.btnRefreshList.Name = "btnRefreshList";
+            this.btnRefreshList.Size = new System.Drawing.Size(156, 39);
+            this.btnRefreshList.TabIndex = 4;
+            this.btnRefreshList.Text = "Refresh List";
+            this.btnRefreshList.UseVisualStyleBackColor = true;
+            this.btnRefreshList.Click += new System.EventHandler(this.btnRefreshList_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(492, 325);
+            this.Controls.Add(this.btnRefreshList);
+            this.Controls.Add(this.btnAddPerson);
+            this.Controls.Add(this.listPeopleListBox);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.tbFirstName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.ListBox listPeopleListBox;
+        private System.Windows.Forms.Button btnAddPerson;
+        private System.Windows.Forms.Button btnRefreshList;
     }
 }
 
--- SQLiteDemo/Form1.cs
+++ SQLiteDemo/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,48 @@ namespace SQLiteDemo
 {
     public partial class Form1 : Form
     {
+        List<PersonModel> people = new List<PersonModel>();
+
         public Form1()
         {
             InitializeComponent();
+
+            LoadPeopleList();
+        }
+
+        private void LoadPeopleList()
+        {
+            people.Add(new PersonModel { FirstName = "Tim", LastName = "Kowal" });
+            people.Add(new PersonModel { FirstName = "John", LastName = "Doe" });
+            people.Add(new PersonModel { FirstName = "Mary", LastName = "Smith" });
+
+            WireUpPeopleList();
+        }
+
+        private void WireUpPeopleList()
+        {
+            listPeopleListBox.DataSource = null;
+            listPeopleListBox.DataSource = people;
+            listPeopleListBox.DisplayMember = "FullName";
+
+        }
+
+        private void btnRefreshList_Click(object sender, EventArgs e)
+        {
+            //LoadPeopleList();
+        }
+
+        private void btnAddPerson_Click(object sender, EventArgs e)
+        {
+            PersonModel p = new PersonModel();
+            p.FirstName = tbFirstName.Text;
+            p.LastName = tbLastName.Text;
+
+            people.Add(p);
+            WireUpPeopleList();
+
+            tbFirstName.Text = "";
+            tbLastName.Text = "";
         }
     }
 }
